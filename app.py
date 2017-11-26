61c342cbf165f89c7ee8ceccb7b047e5375cafe0
ajvani <anmol.jadvani@gmail.com>
2017-11-25 19:21:49 -0500

    adding recording

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app: PhoneBuzz
 '''
 
 import os
-import env
+import time
 from twilio.twiml.voice_response import Gather, VoiceResponse, Say
+from twilio.rest import Client
 from sqlite3 import dbapi2 as sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -22,13 +23,12 @@ def say_fizzbuzz():
 
     response = VoiceResponse()
 
-    msg = "...".join(["FizzBuzz" if x % 3 == 0 and x % 5 == 0 else\
+    msg = "........".join(["FizzBuzz" if x % 3 == 0 and x % 5 == 0 else\
             "Fizz" if x % 3 == 0 else "Buzz" if x % 5 == 0 else str(x)\
-            for x in range(1, n)]) + "... End Fizzbuzz"
+            for x in range(1, n)]) + "................. End Fizzbuzz"
 
-    response.say(msg, voice="woman")
+    response.say(msg)
 
-    print(str(response))
     return str(response)
 
 @app.route('/handle_incoming', methods=['GET','POST'])
@@ -42,15 +42,34 @@ def handle_incoming():
     response.append(gather)
     response.say('Thank you for using PhoneBuzz')
 
-
-    print(str(request.values))
-    print(str(request.values.keys()))
-    print(str(request.values.values()))
-    print(str(request.values.get('Digits', None)))
-    
     return str(response)
 
-    
+@app.route('/handle_outgoing', methods=['GET', 'POST'])
+def handle_outgoing(): 
+    delay = int(request.values.get('delay', 0))
+    number = request.values.get('phone_number', None)
+
+    # putting time delay
+    time.sleep(delay)
+
+    client = Client(ACC_SID, AUTH_TOK)
+    rscm = 'https://127.0.0.1/handle_db_update'
+
+    call = client.calls.create(
+        to=number,
+        from_='+12013406597', 
+        url='https://127.0.0.1/handle_incoming',
+        record=True,
+        recording_status_callback=rscm
+    )
+
+    return redirect('/')
+
+@app.route('/handle_db_update', methods=['GET', 'POST'])
+def handle_db_update(): 
+    print(request.values)
+
+    return "none"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
--- static/js/main.js
+++ static/js/main.js
@@ -19,20 +19,65 @@ $(document).ready(function() {
         var re = /^[0-9]{10}|\([0-9]{3}\) ?[0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
 
         if ($(this).val().match(re) == $(this).val()) {
-            $(this).css('border-color', '#0db71c');
+            $(this).removeClass('invalid-button');
+            $(this).addClass('valid-button');
         } else {
-            $(this).css('border-color', '#b70e0e'); 
+            $(this).removeClass('valid-button');
+            $(this).addClass('invalid-button');
         }
+
+        update_button();
     });
 
     $('#delay').on('input', function() {
         var re = /^[0-9]+$/;
 
         if ($(this).val() == '' || $(this).val().match(re)) {
-            $(this).css('border-color', '#0db71c');
+            $(this).removeClass('invalid-button');
+            $(this).addClass('valid-button');
         } else {
-            $(this).css('border-color', '#b70e0e'); 
+            $(this).removeClass('valid-button');
+            $(this).addClass('invalid-button');
         }
-    })
+
+        update_button();
+    });
+
+    $('#receive_button').on('click', function() {
+        var delay = $('#delay').val();
+        
+        if (delay == '') {
+            delay = '0';
+        }
+
+        $('#success_message').show();
+    });
+
+    $('#receive_form').submit(function() {
+        var delay = $('#delay').val();
+        
+        if (delay == '') {
+            delay = '0';
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/handle_outgoing',
+            data: {
+                phone_number: $('#phone_number').val(),
+                delay: delay
+            }
+        });
+
+        return false;
+    });
 });    
+
+function update_button() {
+    if ($('#phone_number').hasClass('invalid-button') || $('#delay').hasClass('invalid-button')) {
+        $('#receive_button').addClass('disabled-button');
+    } else {
+        $('#receive_button').removeClass('disabled-button');
+    }
+}
 
